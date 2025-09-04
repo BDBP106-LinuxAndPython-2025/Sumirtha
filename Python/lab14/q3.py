caae5516deb34b7282ff73e3ceb707b368011c2f
Sumirtha <sumirthajoyce@gmail.com>
2025-09-04 17:02:05 +0530

    Python starts here

---
+++ Python/lab14/q3.py
@@ -0,0 +1,19 @@
+#Input two integers k,n
+#Output arithemetic operations between k and n
+k=1
+n=10
+summ=k+n
+diff=k-n
+product=k*n
+div=k/n
+expo=k**n
+intdiv=k//n
+mod=k%n
+
+print(summ)
+print(diff)
+print(product)
+print(div)
+print(expo)
+print(intdiv)
+print(mod)
---
+++ Python/lab14/q4.py
@@ -0,0 +1,13 @@
+#Input three integers a,b,c
+#Print square root
+import math
+import cmath
+a=1
+b=15
+c=5
+x=(-b + (math.sqrt((b**2)-(4*a*c))))/(2*a)
+x1=(-b - (math.sqrt((b**2)-(4*a*c))))/(2*a)
+
+print(x)
+print(x1)
+
